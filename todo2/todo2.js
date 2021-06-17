337d9913cd70e6c76cdc85c264dba26408ada770
Daniel Thompson <dthompson0190@gmail.com>
2021-06-16 20:23:08 -0500

    added SLL

--- todo2/todo2.js
+++ todo2/todo2.js
@@ -32,3 +32,29 @@ function rotateArr(arr, shiftBy) {
     }
     return arr
 }
+
+//filter range
+function filterRange(arr, min, max) {
+	for(let i = 0; i < arr.length; i++) {
+		if(arr[i] > min && arr[i] < max) {
+			for(let j = i; j < arr.length - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr.length = arr.length - 1;
+			i--;
+		}
+	}
+}
+
+
+//concat
+function concat(arr1, arr2) {
+    let newArr= [];
+    for (let i = 0; i < arr1.length; i++) {
+        newArr.push(arr1[i])
+    }
+    for (let i = 0; i < arr2.length; i++) {
+        newArr.push(arr2[i])
+    }
+    return newArr
+}
